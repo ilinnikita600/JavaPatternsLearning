2756b3be19c1ef2a1c848c1b42dcebbb61309259
Nikita Ilyin <ilinnikita600@gmail.com>
2022-08-20 17:56:18 +0300

    initial FactoryPattern file and Pizza ingredient Factory interface

---
+++ src/FactoryPattern/PizzaIngredientFactory.java
@@ -0,0 +1,10 @@
+package FactoryPattern;
+
+public interface PizzaIngredientFactory {
+    Dough createDough();
+    Sauce createSauce();
+    Cheese createCheese();
+    Veggies[] createVeggies();
+    Pepperoni createPepperoni();
+    Clams createClam();
+}
