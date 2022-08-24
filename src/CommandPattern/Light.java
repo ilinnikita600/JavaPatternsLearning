7638ebb1033a254fae2533684e1e88f0a7e08a6c
Nikita Ilyin <ilinnikita600@gmail.com>
2022-08-24 22:46:09 +0300

    add Light and command that turn on the light

---
+++ src/CommandPattern/Light.java
@@ -0,0 +1,10 @@
+package CommandPattern;
+
+public class Light {
+    public void on() {
+        System.out.println("Свет включен");
+    }
+    public void off() {
+        System.out.println("Свет выключен");
+    }
+}
