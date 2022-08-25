bc1d3c30c5c128b46359a1e3ba67d6c5e14fe3c0
Nikita Ilyin <ilinnikita600@gmail.com>
2022-08-25 16:16:28 +0300

    add light class and command that turn on light

---
+++ src/CommandPattern2/LightOnCommand.java
@@ -0,0 +1,11 @@
+package CommandPattern2;
+
+public class LightOnCommand implements Command {
+    private final Light light;
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+    public void execute() {
+        light.on();
+    }
+}
